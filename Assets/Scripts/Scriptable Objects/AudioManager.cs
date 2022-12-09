e66f3036f86a2e2c9c4830e39e751efcbf28c425
Maarten  Harms <44709689+maartenharms@users.noreply.github.com>
2022-12-09 09:20:25 +0100

    Audio Manager
    Scriptable Objects baby

---
+++ Assets/Scripts/Scriptable Objects/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[CreateAssetMenu]
+public class AudioManager : ScriptableObject
+{
+	public AudioMixerGroup SFX;
+	public Sound[] Sounds;
+
+	public void Play(string Name)
+	{
+		var s = Array.Find(Sounds, sound => sound.Name == Name);
+		if (s.Source == null)
+		{
+			var reference = new GameObject(s.Name + " (Instance)");
+			var source = reference.AddComponent<AudioSource>();
+			reference.AddComponent<DontDestroySelf>();
+			source.clip = s.Clip;
+			source.volume = s.Volume;
+			source.pitch = s.Pitch;
+			source.playOnAwake = false;
+			source.outputAudioMixerGroup = SFX;
+			s.Source = source;
+		}
+
+		s.Source.Play();
+	}
+
+	public void PlayPing() => Play("Ping");
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Sound
+{
+	public string Name;
+	public AudioClip Clip;
+	[Range(0f, 1f)] public float Volume;
+	[Range(.1f, 3)] public float Pitch;
+
+	[NonSerialized] public AudioSource Source;
+}
