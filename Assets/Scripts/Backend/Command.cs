bfffcadec159ef5a6a858d3409752b278652c318
JYGroman <yorelgroman@gmail.com>
2022-12-28 23:09:55 +0100

    Added greybox mechanics
    Moving car left/right
    Decelerating
    Toggling headlights
    For now tied to keyboard inputs

---
+++ Assets/Scripts/Backend/Command.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Commands 
+{
+    public class Command
+    {
+        public int actorID;
+
+        public virtual void Excecute() 
+        {
+        // Excecute code
+        }
+    }
+
+}
