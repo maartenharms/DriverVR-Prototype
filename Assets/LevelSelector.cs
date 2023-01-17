e5ff5df8f51e7816bb0c6f9c6ad3d49414501a93
BreathingPotato <97896050+BreathingPotato@users.noreply.github.com>
2023-01-17 22:42:56 +0100

    Main Menu Fixes and Button Integration

---
+++ Assets/LevelSelector.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public void Select (string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
